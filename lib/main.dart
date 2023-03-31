3d09ff545f925febf6904d7e0dea7f7c0191ffae
jonas <jonasaustinland@tamu.edu>
2023-03-31 08:32:25 -0500

    Revert "trying again"
    This reverts commit 543c10d1380cf5d53038150ae9ac662f0aabc928.

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:csce315_project3_13/Constants/constants.dart';
+import 'package:csce315_project3_13/GUI/Pages/Test%20Pages/Win_Functions_Test_Page.dart';
+import 'package:csce315_project3_13/GUI/Pages/Win_Manager_View.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'GUI/Pages/Login/Win_Login.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smoothie King App',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      routes:  <String, WidgetBuilder>{
+        Win_Login.route: (BuildContext context) => Win_Login(),
+        Win_Manager_View.route: (BuildContext context) => Win_Manager_View(),
+        Win_Functions_Test_Page.route: (BuildContext context) => Win_Functions_Test_Page(),
+      },
+      initialRoute:  Win_Login.route,
+    );
+  }
+}
+
