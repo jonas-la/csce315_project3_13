916555f8278fe422fd8898a0ca116bb04ef92111
jonas <jonasaustinland@tamu.edu>
2023-04-02 16:10:32 -0500

    getuid function and login functions working

--- functions/index.js
+++ functions/index.js
@@ -5,13 +5,7 @@ const functions = require('firebase-functions');
 const { Client } = require('pg');
 
 //This creates an object we can use to connect to our database
-const client = new Client({
-      host: 'csce-315-db.engr.tamu.edu',
-      user: 'csce315331_team_13_master',
-      password: 'Lucky_13',
-      database: 'csce315331_team_13',
-      port: 5432,
-    });
+
 
 // This is a function without parameters, it's very similar to a parameterized function
 // If you want to create a new function you should replace getEmployeesTest with the function name
@@ -25,6 +19,14 @@ const client = new Client({
 // data and context are auto assigned by system variables if not given so don't stress about them being here
 // also we likely won't use context
 exports.getEmployeesTest = functions.https.onCall(async (data, context) => {
+
+const client = new Client({
+      host: 'csce-315-db.engr.tamu.edu',
+      user: 'csce315331_team_13_master',
+      password: 'Lucky_13',
+      database: 'csce315331_team_13',
+      port: 5432,
+    });
 
 // await makes the rest of the function wait until this line completes
 // this connects to our database
@@ -47,6 +49,14 @@ exports.getEmployeesTest = functions.https.onCall(async (data, context) => {
 // this is a function with a parameter
 exports.getOneEmployeeByIdTest = functions.https.onCall(async (data, context) => {
 
+const client = new Client({
+      host: 'csce-315-db.engr.tamu.edu',
+      user: 'csce315331_team_13_master',
+      password: 'Lucky_13',
+      database: 'csce315331_team_13',
+      port: 5432,
+    });
+
 //  this is makes the function have the parameter employee_id
     const {employee_id} = data;
 
@@ -64,6 +74,14 @@ exports.getOneEmployeeByIdTest = functions.https.onCall(async (data, context) =>
 
 // for getting the user's info after logging in
 exports.getEmployeeByUID = functions.https.onCall(async (data, context) => {
+
+const client = new Client({
+      host: 'csce-315-db.engr.tamu.edu',
+      user: 'csce315331_team_13_master',
+      password: 'Lucky_13',
+      database: 'csce315331_team_13',
+      port: 5432,
+    });
 
     const {employee_uid} = data;
 
--- lib/Services/login_helper.dart
+++ lib/Services/login_helper.dart
@@ -6,13 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class login_helper{
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
-  // late FirebaseFunctions my_functions;
-
-  // login_helper(){
-  //   my_functions = FirebaseFunctions.instance;
-  // }
-
 
   void login({required BuildContext context, required String username, required String password}) async {
     bool sign_in_successful = await sign_in_email_password(user_email: username, user_password: password);
@@ -67,39 +60,36 @@ class login_helper{
 
     String user_uid = await get_firebase_uid();
 
-    employee current_employee = await get_employee_by_uid(user_uid: user_uid);
+    employee current_employee = await get_employee_by_UID_database(user_uid);
 
     print("Current employee's role = ");
     print(current_employee.role);
 
     if(current_employee.role == "Manager"){
-    //  TODO Add navigation to manager page
+    //  TODO Add navigation to Manager page
+      print("Navigating to manager page");
 
     }else if(current_employee.role == "Server"){
-    //  TODO Add navigation to server page
+    //  TODO Add navigation to Server page
+      print("Navigating to server page");
+
+    }else if(current_employee.role == "Menu"){
+      //  TODO Add navigation to Menu page
+      print("Navigating to menu page");
 
     }else if(current_employee.role == "Customer"){
     //  TODO Add navigation to Customer page
+      print("Navigating to customer page");
 
     }else{
       // TODO Handle the edge case
       print("!!! The role was not one of the options");
+      print("Handling unknown navigation");
 
     }
 
-    // TODO remove when function complete
+    // TODO remove when redirects are complete
     Navigator.pushReplacementNamed(context, Win_Manager_View.route);
-  }
-
-
-  Future<employee> get_employee_by_uid({required String user_uid}) async {
-    //TODO get employee from psql database
-
-    employee employee_to_return = employee(id: 0, name: "", email: "", role: "", uid: "", hourly_rate: 0.0);
-
-    employee_to_return = await get_employee_by_UID_database(user_uid);
-
-    return employee_to_return;
   }
 
 
@@ -199,7 +189,7 @@ class login_helper{
   }
 
   Future<String> get_firebase_email() async {
-    // gets the UID of the signed in user from firebase users
+    // gets the email of the signed in user from firebase users
 
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
@@ -220,93 +210,10 @@ class login_helper{
     await FirebaseAuth.instance.signOut();
   }
 
-  Future<employee> get_employee_by_UID_hardcoded(String employee_uid) async {
-    //TODO temporarily rely on this
-    // Temporary function to get the hardcoded value
 
-    Map<String, employee> employee_hash_map = {
-      'RPhPjO2LQ2MMTHMwZS7N3JWFOAx2': employee(
-        id: 1,
-        name: 'Maharshi',
-        email: '',
-        role: 'Manager',
-        uid: 'RPhPjO2LQ2MMTHMwZS7N3JWFOAx2',
-        hourly_rate: 25.87,
-      ),
-      'wQI6aAGC4DYDHoNGXRNORnSnasb2': employee(
-        id: 2,
-        name: 'Jonas',
-        email: '',
-        role: 'Server',
-        uid: 'wQI6aAGC4DYDHoNGXRNORnSnasb2',
-        hourly_rate: 14.99,
-      ),
-      'FjOPjWpPRNYl2W6XqFymugBlKcs2': employee(
-        id: 3,
-        name: 'Tyler',
-        email: '',
-        role: 'Server',
-        uid: 'FjOPjWpPRNYl2W6XqFymugBlKcs2',
-        hourly_rate: 14.99,
-      ),
-      'kvrLmJjlxDgupk3IS6fgpO6KRM33': employee(
-        id: 4,
-        name: 'Freddy',
-        email: '',
-        role: 'Server',
-        uid: 'kvrLmJjlxDgupk3IS6fgpO6KRM33',
-        hourly_rate: 14.99,
-      ),
-      '2XkBeUBFQ9TjEYr2rOjwaqJkGFm2': employee(
-        id: 5,
-        name: 'Seth',
-        email: '',
-        role: 'Manager',
-        uid: '2XkBeUBFQ9TjEYr2rOjwaqJkGFm2',
-        hourly_rate: 25.87,
-      ),
-    };
-
-    employee current_employee = employee(id: 0, name: "", email: "", role: "Customer", uid: employee_uid, hourly_rate: 0.0);
-
-    if(employee_hash_map[employee_uid] != null){
-      current_employee = employee_hash_map[employee_uid] as employee;
-    }
-
-    String employee_email = await get_firebase_email();
-
-    print(employee_email);
-
-    current_employee.email = employee_email;
-
-    return current_employee;
-
-
-
-    // print(employee_id);
-      // print(employee_name);
-      // print(employee_role);
-      // print(employee_uid);
-      // print(employee_hourly_rate);
-
-
-      // int employee_id = int.parse(employee_id_string);
-      // double employee_hourly_rate = double.parse(employee_hourly_rate_string);
-
-      // return employee(id: employee_id, name: employee_name, email: employee_email, role: employee_role, uid: employee_uid, hourly_rate: employee_hourly_rate);
-
-
-  }
-
-
-
-
-//  These are functions to revisit once I figure out what is going on with firebase cloud functios
-// TODO figure out how to query for an employee with firebase functions
-
+  // Queries the psql database to get the data from the employee table
+  // returns an employee object populated by the row with the given uid
   Future<employee> get_employee_by_UID_database(String employee_uid) async {
-    //TODO get this working
-    //It currently has an internal firebase error each time I call it
 
     final HttpsCallable callable = FirebaseFunctions.instance.httpsCallable('getEmployeeByUID');
 
@@ -318,23 +225,23 @@ class login_helper{
 
       // Extract the name of the employee from the data returned by the function
       List<dynamic> employeeData = results.data;
-      int employee_id = employeeData[0]['employee_id'];
+
       String employee_name = employeeData[0]['employee_name'];
       String employee_role = employeeData[0]['employee_role'];
-      double employee_hourly_rate = employeeData[0]['hourly_rate'];
+      int employee_id = employeeData[0]['employee_id'];
 
-      print(employee_id);
-      print(employee_name);
-      print(employee_role);
-      print(employee_uid);
-      print(employee_hourly_rate);
+      String employee_hourly_rate_string = employeeData[0]['hourly_rate'];
+      employee_hourly_rate_string = employee_hourly_rate_string.replaceAll('\$', '');
+      double employee_hourly_rate = double.parse(employee_hourly_rate_string);
 
       String employee_email = await get_firebase_email();
 
+      print(employee_name);
+      print(employee_role);
       print(employee_email);
-
-      // int employee_id = int.parse(employee_id_string);
-      // double employee_hourly_rate = double.parse(employee_hourly_rate_string);
+      print(employee_id);
+      print(employee_uid);
+      print(employee_hourly_rate_string);
 
       return employee(id: employee_id, name: employee_name, email: employee_email, role: employee_role, uid: employee_uid, hourly_rate: employee_hourly_rate);
 
@@ -343,50 +250,83 @@ class login_helper{
       print("error was");
       print(e);
 
-
       return employee(id: 0, name: "", email: "", role: "Customer", uid: employee_uid, hourly_rate: 0.0);
     }
-
     }
 
 
-//   Future<void> modify_results(String employee_uid) async {
-//     // final my_results = await get_employee_by_UID(employee_uid);
-//     print("got employee");
-//   }
+// These functions were used for testing getting an employee
+// Future<employee> get_employee_by_uid({required String user_uid}) async {
+//   // calls the get_employee_by_UID_database function and returns the results
 //
-//   Future<void> test_id() async {
-//     try{
-//       final HttpsCallable callable = my_functions.httpsCallable('getOneEmployeeByIdTest');
+//   employee employee_to_return = employee(id: 0, name: "", email: "", role: "", uid: "", hourly_rate: 0.0);
 //
-// // Call the function with the employee UID as input
-//       final results = await callable.call(<String, dynamic>{
-//         'employee_id': 2,
-//       });
+//   employee_to_return = await get_employee_by_UID_database(user_uid);
 //
-// // Extract the name of the employee from the data returned by the function
-//       List<dynamic> employeeData = results.data;
-//       String employeeName = employeeData[0]['employee_name'];
-//       print(employeeName);
-//     }catch(e){
-//       print(e);
-//     }
-//
-//   }
-//
-//   Future<void> test_uid() async {
-//     final HttpsCallable callable = my_functions.httpsCallable('getEmployeeByUID');
-//
-// // Call the function with the employee UID as input
-//     final results = await callable.call(<String, dynamic>{
-//       'employee_uid': 'wQI6aAGC4DYDHoNGXRNORnSnasb2',
-//     });
-//
-// // Extract the name of the employee from the data returned by the function
-//     List<dynamic> employeeData = results.data;
-//     String employeeName = employeeData[0]['employee_name'];
-//     print(employeeName);
-//   }
-//
+//   return employee_to_return;
+// }
+
+
+  // This was temporary to get the hard coded values of the employees
+  // Future<employee> get_employee_by_UID_hardcoded(String employee_uid) async {
+  //
+  //
+  //   Map<String, employee> employee_hash_map = {
+  //     'RPhPjO2LQ2MMTHMwZS7N3JWFOAx2': employee(
+  //       id: 1,
+  //       name: 'Maharshi',
+  //       email: '',
+  //       role: 'Manager',
+  //       uid: 'RPhPjO2LQ2MMTHMwZS7N3JWFOAx2',
+  //       hourly_rate: 25.87,
+  //     ),
+  //     'wQI6aAGC4DYDHoNGXRNORnSnasb2': employee(
+  //       id: 2,
+  //       name: 'Jonas',
+  //       email: '',
+  //       role: 'Server',
+  //       uid: 'wQI6aAGC4DYDHoNGXRNORnSnasb2',
+  //       hourly_rate: 14.99,
+  //     ),
+  //     'FjOPjWpPRNYl2W6XqFymugBlKcs2': employee(
+  //       id: 3,
+  //       name: 'Tyler',
+  //       email: '',
+  //       role: 'Server',
+  //       uid: 'FjOPjWpPRNYl2W6XqFymugBlKcs2',
+  //       hourly_rate: 14.99,
+  //     ),
+  //     'kvrLmJjlxDgupk3IS6fgpO6KRM33': employee(
+  //       id: 4,
+  //       name: 'Freddy',
+  //       email: '',
+  //       role: 'Server',
+  //       uid: 'kvrLmJjlxDgupk3IS6fgpO6KRM33',
+  //       hourly_rate: 14.99,
+  //     ),
+  //     '2XkBeUBFQ9TjEYr2rOjwaqJkGFm2': employee(
+  //       id: 5,
+  //       name: 'Seth',
+  //       email: '',
+  //       role: 'Manager',
+  //       uid: '2XkBeUBFQ9TjEYr2rOjwaqJkGFm2',
+  //       hourly_rate: 25.87,
+  //     ),
+  //   };
+  //
+  //   employee current_employee = employee(id: 0, name: "", email: "", role: "Customer", uid: employee_uid, hourly_rate: 0.0);
+  //
+  //   if(employee_hash_map[employee_uid] != null){
+  //     current_employee = employee_hash_map[employee_uid] as employee;
+  //   }
+  //
+  //   String employee_email = await get_firebase_email();
+  //
+  //   print(employee_email);
+  //
+  //   current_employee.email = employee_email;
+  //
+  //   return current_employee;
+  // }
 
 }
